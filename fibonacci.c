75e3b33c2bb1973db113960bb9aebb0c5f60675d
whitebear <80818534+whitebear05@users.noreply.github.com>
2021-04-12 11:03:42 +0900

    Create fibonacci.c

---
+++ fibonacci.c
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int fibonacci(int); // 함수 선언
+
+int main(void)
+{
+	int n;
+	scanf("%d", &n);
+	fibonacci(n);
+
+	return 0;
+}
+
+int fibonacci(int n) // 함수 정의
+{
+	// 0 1 1 2 3 5 8 ...
+	int f1 = 0, f2 = 1, f3, i;
+	if (n == 1) // 첫 번째는 0 리턴
+		printf("%d", f1);
+	else
+		printf("%d %d", f1, f2);
+
+	for (i = 0; i < n - 2; i++)
+	{
+		f3 = f1 + f2;
+		printf("%d", f3);
+		f1 = f2;
+		f2 = f3;
+	}
+}
