61d43a9ba8634ff8731b7148046d9a88bf74f747
whitebear <80818534+whitebear05@users.noreply.github.com>
2021-04-12 11:06:16 +0900

    Update fibonacci.c

--- fibonacci.c
+++ fibonacci.c
@@ -17,14 +17,14 @@ int fibonacci(int n) // 함수 정의
 	// 0 1 1 2 3 5 8 ...
 	int f1 = 0, f2 = 1, f3, i;
 	if (n == 1) // 첫 번째는 0 리턴
-		printf("%d", f1);
+		printf("%d ", f1);
 	else
-		printf("%d %d", f1, f2);
+		printf("%d %d ", f1, f2);
 
 	for (i = 0; i < n - 2; i++)
 	{
 		f3 = f1 + f2;
-		printf("%d", f3);
+		printf("%d ", f3);
 		f1 = f2;
 		f2 = f3;
 	}
