5093f7ca2dd4f62fea8497e4ebcb88cee9412e47
whitebear <80818534+whitebear05@users.noreply.github.com>
2021-04-19 10:52:39 +0900

    Create ReverseString1.c

---
+++ ReverseString1.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+// 문자열을 입력받아 문자열을 뒤집어 출력하는 프로그램을 작성하시오
+// 입력 예 : Hello
+// 출력 예 : olleH
+// 힌트 : 널문자, 배열의 인덱스 활용
+
+int main(void)
+{
+	char str[100];
+	int i, end = 0;
+
+	scanf("%s", str);
+
+	for (i = 0; i < sizeof(str); i++)
+	{
+		if (str[i] == '\0')
+		{
+			end = i;
+			break;
+		}
+	}
+
+	for (i = end; i >= 0; i--)
+		printf("%c", str[i]);
+
+	return 0;
+}
