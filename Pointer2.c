c36dc20d11f6a8f389f56b98e56b059bc2214084
whitebear <80818534+whitebear05@users.noreply.github.com>
2021-04-26 10:43:58 +0900

    Update Pointer2.c

--- Pointer2.c
+++ Pointer2.c
@@ -7,7 +7,7 @@ int main(void)
 	p = &n1;
 
 	printf("주소(Address) : %p, %p\n", p, &n1);
-	printf("내용(혹은 값)  : %d, %d\n", *p, n1);
+	printf("내용(Value)   : %d, %d\n", *p, n1);
 
 	for (int i = 0; i < 5; i++)
 		printf("%4d", (*p)++);
