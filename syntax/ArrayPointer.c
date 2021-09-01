a81d4896666e3009df71a67d63bfe69a2b6250d4
whitebear <80818534+whitebear05@users.noreply.github.com>
2021-09-01 11:18:51 +0900

    Create ArrayPointer.c

---
+++ syntax/ArrayPointer.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int* p;			// 1차원 포인터
+					// p는 어떤 변수의 주소를 저장하는 변수
+	int (*ptr)[5];	// 배열 포인터
+	int arr[5];		// 1차원 배열
+
+	p = arr;
+	ptr = &arr;
+
+	printf("p=%p, ptr=%p\n", p, ptr);
+
+	p++; // int형은 4byte이니까 4byte 증가
+	ptr++; // 4byte*5개 = 20byte 증가
+
+	printf("p=%p, ptr=%p\n", p, ptr);
+
+	return 0;
+}
